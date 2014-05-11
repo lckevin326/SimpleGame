001c8d5d5dd8ec127ab1e8d17bac006064eb62ec
Kevin <lckevin714@gmail.com>
2014-05-11 21:37:43 +0800

    Initial Commit

---
+++ SimpleGame/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  SimpleGame
+//
+//  Created by LiuChao on 14-5-11.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ SimpleGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SimpleGame
+//
+//  Created by LiuChao on 14-5-11.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
